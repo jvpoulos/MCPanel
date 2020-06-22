2991f87f16f8bb3871b0076b2a758c70c7c651b4
Jason Poulos <jvpoulos@gmail.com>
2020-06-22 13:11:12 -0400

    elementwise CB multiplication

--- README.md
+++ README.md
@@ -43,9 +43,8 @@ weights <- matrix(NA, N, T) # transform weights for regression
 weights[c(4,5),] <- 1/(W[c(4,5),]) # treated group
 weights[-c(4,5),] <- 1/(1-W[-c(4,5),]) # control group
 
-est_model_MCPanel_w <- mcnnm_wc_fit(M = Y_obs, C = weights, mask = treat_mat, W= weights, lambda_L=0.1, lambda_B=0.1, to_normalize = 1L,
-  to_estimate_u = 1L, to_estimate_v = 1L, niter = 100L,
-  rel_tol = 1e-05, is_quiet = 1L)
+est_model_MCPanel_w <- mcnnm_wc_cv(M = Y_obs, C = weights, mask = treat_mat, W= weights, 
+	to_normalize = 1, to_estimate_u = 1, to_estimate_v = 1, num_lam_L = 5, num_lam_B = 5, niter = 500, rel_tol = 1e-05, cv_ratio = 0.8, num_folds = 3, is_quiet = 1)
 
 est_model_MCPanel_w$Mhat <- est_model_MCPanel_w$L + weights*est_model_MCPanel_w$B + replicate(T,est_model_MCPanel_w$u) + t(replicate(N,est_model_MCPanel_w$v))
 ```
--- src/mcnnm_R.cpp
+++ src/mcnnm_R.cpp
@@ -81,7 +81,7 @@ NumericMatrix ComputeMatrix_B(NumericMatrix L, NumericMatrix C, NumericMatrix B,
   int num_rows = L_.rows();
   int num_cols = L_.cols();
 
-  MatrixXd res_ = L_ + C_ * B_ + u_ * VectorXd::Constant(num_cols,1).transpose() + VectorXd::Constant(num_rows,1) * v_.transpose();
+  MatrixXd res_ = L_ + C_.cwiseProduct(B_) + u_ * VectorXd::Constant(num_cols,1).transpose() + VectorXd::Constant(num_rows,1) * v_.transpose();
 
   return wrap(res_);
 }
@@ -286,7 +286,7 @@ NumericVector update_u_B(NumericMatrix M, NumericMatrix C, NumericMatrix B, Nume
   const Map<MatrixXd> C_(as<Map<MatrixXd> >(C));
   const Map<MatrixXd> B_(as<Map<MatrixXd> >(B));
   const Map<VectorXd> v_(as<Map<VectorXd> >(v));
-  MatrixXd T_ = C_ * B_;
+  MatrixXd T_ = C_.cwiseProduct(B_);
 
   VectorXd res(M_.rows(),1);
   for (int i = 0; i<M_.rows(); i++){
@@ -341,7 +341,7 @@ NumericVector update_v_B(NumericMatrix M, NumericMatrix C, NumericMatrix B, Nume
   const Map<MatrixXd> C_(as<Map<MatrixXd> >(C));
   const Map<MatrixXd> B_(as<Map<MatrixXd> >(B));
   const Map<VectorXd> u_(as<Map<VectorXd> >(u));
-  MatrixXd T_ = C_ * B_;
+  MatrixXd T_ = C_.cwiseProduct(B_);
 
   VectorXd res(M_.cols(),1);
   for (int i = 0; i<M_.cols(); i++)
@@ -474,7 +474,6 @@ List initialize_uv_B(NumericMatrix M, NumericMatrix C, NumericMatrix mask, Numer
     using Eigen::Map;
     const Map<MatrixXd> M_(as<Map<MatrixXd> >(M));
     const Map<MatrixXd> mask_(as<Map<MatrixXd> >(mask));
-    const Map<MatrixXd> C_(as<Map<MatrixXd> >(C));
     int num_rows = M_.rows();
     int num_cols = M_.cols();
 
@@ -666,7 +665,6 @@ List NNM_fit_B(NumericMatrix M, NumericMatrix C, NumericMatrix B_init, NumericMa
   // matrices L, B, u, and v are updated till convergence via coordinate descent.
   using Eigen::Map;
   const Map<MatrixXd> M_(as<Map<MatrixXd> >(M));
-  const Map<MatrixXd> C_(as<Map<MatrixXd> >(C));
   int num_rows = M_.rows();
   int num_cols = M_.cols();
 
